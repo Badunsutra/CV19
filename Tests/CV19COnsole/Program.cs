e01102602bcebae09d2922f96eacc3f54cb10706
Дмитрий Пономарев <ponomarev.dy@yandex.ru>
2022-03-11 13:36:46 +0300

    Запрос к удаленному серверу для получения сырых данных

--- Tests/CV19COnsole/Program.cs
+++ Tests/CV19COnsole/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 
-namespace CV19COnsole
+namespace CV19Console
 {
     internal class Program
     {
+        private const string data_uri = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //WebClient web_client = new WebClient();
+
+            HttpClient client = new HttpClient();
+
+            var response = client.GetAsync(data_uri).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+            Console.ReadLine();
         }
     }
 }
