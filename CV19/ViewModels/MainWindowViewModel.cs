f1b9ed6a601df36f37951afc45c934e722e2e64d
Дмитрий Пономарев <ponomarev.dy@yandex.ru>
2022-03-11 10:37:06 +0300

    Добавление свойства Заголовка в MainWindowViewModel

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using CV19.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CV19.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Title
+        private string _title;
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        } 
+        #endregion
     }
 }
