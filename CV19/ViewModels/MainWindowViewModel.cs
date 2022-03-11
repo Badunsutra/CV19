512eca7f6599dd4fb2f9e91792e628b2fc19ef38
Дмитрий Пономарев <ponomarev.dy@yandex.ru>
2022-03-11 14:40:26 +0300

    Генерация перечесления точек в MainWindowViewModel

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CV19.Infrastructure.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
@@ -10,40 +11,55 @@ namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-        #region Title
-        private string _title = "Анализ статистики CV19";
+        #region TestDataPoints
 
+        private IEnumerable<DataPoint> _testDataPoints;
+        /// <summary>Тестовый набор данных для визуализации графиков</summary>
+        public IEnumerable<DataPoint> TestDataPoints
+        {
+            get => _testDataPoints;
+            set => Set(ref _testDataPoints, value);
+        } 
+
+        #endregion
+
+        #region Title
+
+        private string _title = "Анализ статистики CV19";
         /// <summary>Заголовок окна</summary>
         public string Title
         {
             get => _title;
             set => Set(ref _title, value);
         }
+
         #endregion
 
         #region Status
-        private string _status = "Готов!";
 
+        private string _status = "Готов!";
         /// <summary>Статус программы</summary>
         public string Status
         {
             get => _status;
             set => Set(ref _status, value);
         }
+
         #endregion
 
         #region Команды
 
         #region CloseApplicationCommand
+
         /// <summary>Команда закрытия программы</summary>
         public ICommand CloseApplicationCommand { get; }
-
         private void OnCloseApplicationCommandExecuted(object p)
         {
             Application.Current.Shutdown();
         }
 
         private bool CanCloseApplicationCommandExecute(object p) => true; 
+
         #endregion
 
         #endregion
@@ -56,6 +72,17 @@ namespace CV19.ViewModels
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
 
             #endregion
+
+            var data_points = new List<DataPoint>((int)(360/0.1));
+            for(var x = 0d; x < 360; x += 0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                var y = Math.Sin(x * to_rad);
+
+                data_points.Add(new DataPoint { XValue = x, YValue = y });
+            }
+
+            TestDataPoints = data_points;
         }
     }
 }
