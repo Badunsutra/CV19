3c6e4deb8a7d89df672648a5333111f7993e8b45
Дмитрий Пономарев <ponomarev.dy@yandex.ru>
2022-04-01 10:09:23 +0300

    Дизайн главного окна для работы со списками студентов

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using CV19.Infrastructure.Commands;
 using CV19.Models;
+using CV19.Models.Decanat;
 using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Input;
@@ -11,13 +14,18 @@ namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        public ObservableCollection<Group> Groups { get; }
+
+
+
+        #region SelectedIndex
         private int _selectedIndex;
         public int SelectedIndex
         {
             get => _selectedIndex;
             set => _selectedIndex = value;
-        }
-
+        } 
+        #endregion
 
         #region TestDataPoints
 
@@ -55,6 +63,8 @@ namespace CV19.ViewModels
 
         #endregion
 
+        /*----------------------------------------------------------------------------------------------------------------------------------------*/
+
         #region Команды
 
         #region CloseApplicationCommand
@@ -76,11 +86,12 @@ namespace CV19.ViewModels
         {
             if (p is null) return;
             SelectedIndex += Convert.ToInt32(p);
-        } 
+        }
         #endregion
 
         #endregion
 
+        /*----------------------------------------------------------------------------------------------------------------------------------------*/
 
         public MainWindowViewModel()
         {
@@ -101,6 +112,24 @@ namespace CV19.ViewModels
             }
 
             TestDataPoints = data_points;
+
+            var student_index = 1;
+            var students = Enumerable.Range(1, 10).Select(i => new Student
+            {
+                Name = $"Name {student_index}",
+                Surname = $"Surname {student_index}",
+                Patronymic = $"Patronymic {student_index++}",
+                Birthday = DateTime.Now,
+                Rating = 0
+            });
+
+            var groups = Enumerable.Range(1, 20).Select(i => new Group
+            {
+                Name = $"Группа {i}",
+                Students = new ObservableCollection<Student>(students)
+            });
+
+            Groups = new ObservableCollection<Group>(groups);
         }
     }
 }
